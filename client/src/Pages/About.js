52bc0323e27a24cc00ff0bb7fd2db6799f36bb5f
SamsterB <samanthabrown146@msn.com>
2021-01-05 21:58:18 +0000

    did this work

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <p> Punchology started in our home Kitchen. We started making unches for the family on a Sunday using my old family recipe which I had adapted.</p>
             <p> A friend tasted our signature pumpkin punch and asked if we could make some for hm to buy. The very next day we had our first customer, we posted the picture on snapchat and within 10 mins another friend asked if they could buy some. By the end of the week Punchology was born</p>
             <p> The range continued to develop by adding vegan punches to the range, sea moss gel and our elderberry winter elixir.</p>
-            <p> We work our hardest to bring you quality produce made fresh to order using the finest ingredients</p>
+            <p> We work our hardest to bring you quality produce made fresh to order using the finest ingredients. We shall continue </p>
             <h3>Meet the Team</h3>
              <img src="/images/Lee.jpeg" alt =""/>
              <img src="/images/Sam.jpeg" alt =""/>
