47e131b66b9f90c0393041024a7c42f2c3b77859
SamsterB <samanthabrown146@msn.com>
2021-01-05 21:50:13 +0000

    no longer required

---
+++ client/src/Pages/Punches.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Punches = () => {
+
+    return (
+        <div>
+            <h3> "The discipline of creating the finest Afro-Carribean punch blends."</h3>
+            
+        </div>
+    );
+
+};
+
+export default Punches;
