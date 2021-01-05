6315aabf0cd30c0e0bdee44f1c7c22225bb00239
SamsterB <samanthabrown146@msn.com>
2021-01-05 21:47:46 +0000

    added product data for 3 items

---
+++ client/src/components/data/product_data.js
@@ -0,0 +1,35 @@
+const product_card = [
+    {
+        id:1,
+        product_name:"Pineapple Punch",
+        description:"Our unique blend of pineapple and spices, with a rich creamy taste will leaving you wanting more.",
+        price:12.00,
+        currency:"£",
+        thumb:"./images/1.jpeg",
+
+    },
+
+    {
+
+    id:2,
+    product_name:"Guinness Punch",
+    description:"Our unique blend of Guinness and spices, with a rich creamy taste will leaving you wanting more.",
+    price:12.00,
+    currency:"£",
+    thumb:"./images/2.jpeg",
+
+    },
+
+    {
+
+    id:3,
+    product_name:"Carrot Punch",
+    description:"Our unique blend of carrot and spices, with a rich creamy taste will leaving you wanting more.",
+    price:12.00,
+    currency:"£",
+    thumb:"./images/3.jpeg",
+
+    },
+]
+
+export default product_card;
