b424838616e9440f37ba433da79eb7b41b1a2ca9
SamsterB <samanthabrown146@msn.com>
2021-01-05 21:48:19 +0000

    i think i have deleted this

---
+++ client/src/Pages/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Menu = () => {
+
+    return (
+        <div>
+            <h3> "The discipline of creating the finest Afro-Carribean punch blends."</h3>
+            
+        </div>
+    );
+
+};
+
+export default Menu;
