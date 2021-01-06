7656a5a5e5568aeb8ea085c386af5d3cba530a4b
SamsterB <samanthabrown146@msn.com>
2021-01-06 19:12:54 +0000

    uptated scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./App.css";
+import "./App.scss";
 import Header from "./components/Header";
 import MainContent from "./components/mainContent";
 import Footer from "./components/Footer";
