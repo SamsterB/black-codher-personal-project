5d356b8a621bbb202639f7ad0ff60216c98f2c2b
SamsterB <samanthabrown146@msn.com>
2021-01-13 07:33:40 +0000

    trying to add count for stock levels

---
+++ client/src/components/totalCount.js
@@ -0,0 +1,8 @@
+export const totalCount ={ 
+        "Condense milk": 20,
+        "Pineapple": 10,
+        "Nurishment": 60,
+        "Agave": 80,
+        "Guinness":50, 
+        "Carrot":10,
+}
