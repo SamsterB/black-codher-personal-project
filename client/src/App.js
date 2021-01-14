3e991cd915d57f847a7715b3f94c8344cfe18fbd
SamsterB <samanthabrown146@msn.com>
2021-01-14 21:09:02 +0000

    trying to add the items to the cart. not working at the moment.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import "./App.css";
+import React, { useState } from 'react';
+//import "./App.css";
 import "./App.scss";
 import Header from "./components/Header";
 import Punches from "./Pages/Punches";
 import About from "./Pages/About";
 import Footer from "./components/Footer";
+import Basket from "./components/Basket";
+import { useState } from 'react';
 import {BrowserRouter as Router , Switch, Route} from "react-router-dom";
 
 
 function App() {
+  const [cartItems, setCartItems] = useState ([]);
   return (
     <div className="container">
       <Router>
@@ -19,6 +22,9 @@ function App() {
           </Route>
           <Route path='/'>
             <Punches/>
+          </Route>
+          <Route path='/'>
+            <Basket>cartItems={cartItems}</Basket>
           </Route>
         </Switch>
         
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,19 +4,20 @@ import {Link} from "react-router-dom";
 export default function Header() {
     return (
         <nav>
-            <div>
-                <img src="/images/logo.jpg" alt=""/>
-            </div>
-            <ul>
-                <li> <Link to='/'>Home</Link></li>
-                <li><Link to='/products'>Products</Link></li>
-                <li><Link to='/about'>About Us</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-            <div className="search">
-                <i className="fa fa-search"></i>
-                <i className="fa fa-shopping-basket"></i>
-            </div>
+            <header className="row block centered">
+                <div>
+                    <img src="/images/logo.jpg" alt=""/>
+                </div>
+                <ul>
+                    <li> <Link to='/'>Home</Link></li>
+                    <li><Link to='/products'>Products</Link></li>
+                    <li><Link to='/about'>About Us</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                </ul>
+                <div>
+                    <a href="#/cart">Cart</a>
+                </div>
+            </header>
         </nav>
     )
 }
